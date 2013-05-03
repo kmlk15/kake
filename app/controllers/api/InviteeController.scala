573b441faaed07c5a322d214fe3f03e0694706da
Keith Lui <kmlk15@gmail.com>
2013-05-03 08:18:01 -0700

    for jsgrid

---
+++ app/controllers/api/InviteeController.scala
@@ -0,0 +1,43 @@
+package controllers.api
+
+import play.api._
+import play.api.mvc._
+import play.api.libs.json._
+import base._
+
+object InviteeController extends Controller {
+
+  val invitee = registry inviteeService
+
+  def action = Action { implicit request =>
+    {
+      val map = request.body.asFormUrlEncoded.getOrElse(Map.empty[String, Seq[String]]).map { case (k, v) => k -> v.mkString }
+      
+      play.Logger.info(map.toString)
+      
+      map("oper") match {
+        case "add" => 
+          invitee.add(map)
+          Status(200)
+        case "edit" => 
+          invitee.update(map)
+          Status(200)
+        case _ =>
+          Status(500)
+      }
+    }
+  }
+
+  def get(id: String) = Action {
+    val result = invitee.get(id)
+
+    Ok(Json.toJson(result))
+  }
+
+  def list() = Action {
+    val result = invitee.list()
+
+    Ok(Json.toJson(result))
+  }
+
+}
--- db/setup.sql
+++ db/setup.sql
@@ -2,13 +2,13 @@ create table invitees (
 id int primary key auto_increment,
 englishName varchar(255),
 chineseName varchar(255),
-relationship varchar(1),
+relationship varchar(20),
 gender varchar(1),
 invitedBy varchar(5),
 invitedNum int default 1,
-status varchar(10),
+status varchar(20),
 goingNum int default 0,
 tableNum int default 0,
 giftItem varchar(255),
 giftValue double default 0
-);
+) CHARACTER SET utf8 COLLATE utf8_unicode_ci;
